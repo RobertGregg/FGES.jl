f4e95540e6db3aa04e69b3e91a31a6e731c8fd92
RobertGregg <robwgregg@gmail.com>
2023-05-03 14:59:02 -0700

    Added R Dataframe Example

--- docs/InterfaceR.R
+++ docs/InterfaceR.R
@@ -1,6 +1,10 @@
 #Library to connect julia and R
 library(JuliaConnectoR)
 
+##################################
+#Example 1: raw data matrix
+##################################
+
 #Load the FGES package in from julia
 FGES <- juliaImport("FGES")
 
@@ -20,4 +24,26 @@ FGES$saveGraph("myGraph.txt", g)
 gload <- FGES$loadGraph("myGraph.txt")
 
 #Convert the edge list to an R dataframe
-df <- as.data.frame(FGES$edgetable(g))
+df <- as.data.frame(FGES$edgetable(gload))
+
+##################################
+#Example 2: Dataframes
+##################################
+
+#Load the FGES package in from julia
+FGES <- juliaImport("FGES")
+
+#Import DataFrames from Julia
+DataFrames <- juliaImport("DataFrames")
+
+#Create a random dataframe in R
+df <- as.data.frame(matrix(runif(10000*100) , ncol = 100))
+
+#Convert the R dataframe to a Julia DataFrame
+df <- DataFrames$DataFrame(df)
+
+#Pass the dataframe to FGES
+g <- FGES$fges(df)
+
+#Display the edges found by fges
+FGES$alledges(g)
